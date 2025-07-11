ecd741aae715882146ef2d9c8e054edbad66fdbb
清欢 <167692706+Hu410@users.noreply.github.com>
2025-07-12 03:09:58 +0800

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -12,4 +12,4 @@ if [ ! $(pgrep cloudflared) ]; then
     nohup ~/bin/cloudflared tunnel --edge-ip-version auto --protocol http2 run --token eyJhIjoiZGM5OWQzYTFmNTU0Yjc1ODk4ZGFhN2RmMWNjYWE3MTUiLCJ0IjoiMmFmMGE1YjYtNDBjMi00MDYzLWI4MjUtOTEwYjJkM2ZhMzU5IiwicyI6Ik1XRmhaVFExWW1JdFpUQmpNaTAwWldZNExXSmxOMkl0TVdaaFl6aG1ZMll4TldFeSJ9 > /dev/null 2>&1 &
 fi
 
-~/bin/caddy run --config ":18880 { root * /root; file_server }"
+~/bin/caddy run --config ~/bin/Caddyfile
