063bcd836c917c65d369617f5f71ac4876b9232a
清欢 <167692706+Hu410@users.noreply.github.com>
2025-08-05 22:23:41 +0800

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -9,7 +9,7 @@ if [ ! $(pgrep cloudflared) ]; then
     nohup ~/bin/cloudflared tunnel --url http://127.0.0.1:18881 --no-autoupdate > ~/bin/argo.log 2>&1 &
 fi
 
-sleep 3
-cat argo.log | grep -oE "https://.*[a-z]+cloudflare.com" | sed "s#https://##" > /var/www/index.html
+sleep 10
+cat ~/bin/argo.log | grep -oE "https://.*[a-z]+cloudflare.com" | sed "s#https://##" > /var/www/index.html
 
 ~/bin/caddy run --config ~/bin/Caddyfile
