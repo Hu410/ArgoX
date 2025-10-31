f12593b00823a481b7d115bf5c45f1624a974c0c
尽欢 <167692706+Hu410@users.noreply.github.com>
2025-11-01 01:37:07 +0800

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -9,10 +9,6 @@ tar -zxvf caddy_linux_amd64.tar.gz
 rm -rf README.md LICENSE *.zip *.tar.gz
 chmod +x xray caddy cloudflared
 
-if [ ! $(pgrep cloudflared) ]; then
-    nohup ~/cloudflared tunnel --url http://127.0.0.1:18881 --no-autoupdate > ~/cloudflared.log 2>&1 &
-fi
-
 cat > config.json <<EOF 
 {
   "log": {
@@ -73,17 +69,3 @@ cat > config.json <<EOF
   }
 }
 EOF
-
-echo "vless://ffffffff-ffff-ffff-ffff-ffffffffffff@cf.0sm.com:443?security=tls&sni=trycloudflare.com&allowInsecure=1&type=ws&path=/&host=trycloudflare.com&encryption=none#ArgoX" > /var/www/sub
-
-tunnel_url=$(cat ~/cloudflared.log | grep -oE "https://.*[a-z]+cloudflare.com" | sed "s#https://##")
-
-sub_url=$(sed -E "s/(host=|sni=|&host=)[^&]*/\1$new_url/g" /var/www/sub)
-
-echo $sub_url > /var/www/sub
-
-nohup ~/caddy run --config ~/Caddyfile > /dev/null 2>&1 &
-
-# if [ ! $(pgrep xray) ]; then
-#     nohup ~/bin/xray run -c ~/bin/config.json > ~/run.log 2>&1 &
-# fi
